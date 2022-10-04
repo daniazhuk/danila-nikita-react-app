1683fc82eb36da79e258f2223c9a4f7dab8b907e
nikita <dn200501@gmail.com>
2022-10-04 23:13:36 +0300

    wrote some text to StoreList.tsx

---
+++ src/components/Store/components/StoreList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StoreList = () => {
+    return (
+        <div>
+            <p>STRELIST SUKA!!!!!!!</p>
+        </div>
+    );
+};
+
+export default StoreList;
