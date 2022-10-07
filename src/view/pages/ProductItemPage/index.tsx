a1a98d36f4a7e310b2a7a19f5c0430208a3af518
nikita <dn200501@gmail.com>
2022-10-07 12:50:15 +0300

    modified json to 9 engine items

--- src/view/pages/ProductItemPage/index.tsx
+++ src/view/pages/ProductItemPage/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC, useState, useEffect } from "react";
 import { IProd } from "../../../types";
 
-const ProductItemPage: FC = () => {
+const ProductItemPage: FC<IProd> = () => {
   const [product, setProduct] = useState<IProd | null>(null);
 
   useEffect(() => {
     fetch("store.json")
       .then((response) => response.json())
-      .then((products: IProd[]) => {
+      .then((products: IProd) => {
         setProduct(product);
         console.log(product);
       });
