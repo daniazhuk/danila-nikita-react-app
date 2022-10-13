1c4b7bc413f2e07d4a4bb32f3021527072d3fa95
nikita <dn200501@gmail.com>
2022-10-13 17:01:55 +0300

    I started making sort mechanism, it works through the ass because still has no types

--- src/view/components/Store/index.tsx
+++ src/view/components/Store/index.tsx
@@ -1,9 +1,12 @@
 import React, { FC, useState, useEffect } from "react";
 import { IProd } from "../../../types";
+import Select from "../../common/Select/Select";
 import StoreList from "./components/StoreList";
 
 const Store: FC<IProd> = () => {
   const [products, setProducts] = useState<IProd[]>([]);
+  const [selectedSort, setSelectedSort] = useState()
+
 
   useEffect(() => {
     fetch("store.json")
@@ -14,8 +17,22 @@ const Store: FC<IProd> = () => {
         
       });
   }, []);
+
+  const sortProducts = (sort: any) => {
+    setSelectedSort(sort)
+    setProducts([...products].sort((a,b) => a[sort].localeCompare(b[sort])))
+  }
   return (
     <div>
+      <Select 
+      value={selectedSort}
+      onChange={sortProducts}
+      defaultValue="Sort"
+      options={[
+        {value: "type", name:"by type"},
+        {value: "price", name:"by price"},
+        {value: "name", name:"by name"}
+      ]}/>
       <StoreList products={products}/>
     </div>
   );
