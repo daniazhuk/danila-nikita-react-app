97bfd6c87863010a454b058db371d3d0c27d2c82
nikita <dn200501@gmail.com>
2022-10-22 17:59:53 +0300

    fixed files and styles

--- src/view/components/ScrollToTop/index.tsx
+++ src/view/components/ScrollToTop/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { FaAngleUp } from 'react-icons/fa';
-import "../../../styles/index.css";
+import "./scriollToTop.css";
 
 const ScrollToTop = () => {
     const [showTopBtn, setShowTopBtn] = useState(false)
