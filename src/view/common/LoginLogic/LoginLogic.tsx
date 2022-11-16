11d298665626f0f2977203ab9919f96a40dd84b6
daniazhuk <danilzhuchenko@gmail.com>
2022-11-16 23:22:33 +0200

    created logic of loginform

---
+++ src/view/common/LoginLogic/LoginLogic.tsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import LoginForm from '../../components/LoginForm'
+
+function LoginLogic() {
+    const adminUser = {
+        email: "admin@admin.com",
+        password: "admin123"
+    }
+
+    const [user, setUser] = useState({name: "", email: ""})
+    const [error, setError] = useState("")
+
+    const Login = details => {
+
+        if(details.email == adminUser.email && details.password == adminUser.password) {
+            setUser({
+                name: details.name,
+                email: details.email
+            });
+        } else {
+            setError("Details do not match!")
+        }
+
+    }
+
+    const Logout = () => {
+        setUser({name: "", email: ""})
+    }
+
+    return (
+        <div>
+            {(user.email != "") ? (
+                <div className="welcome">
+                    <h2>Welcome, <span>{user.name}</span></h2>
+                    <button onClick={Logout}>Logout</button>
+                </div>
+
+            ) : (
+                <LoginForm Login={Login} error={error}/>
+            )}
+        </div>
+    );
+}
+
+export default LoginLogic;
--- src/view/pages/HomePage/index.tsx
+++ src/view/pages/HomePage/index.tsx
@@ -2,11 +2,13 @@ import React, {FC} from 'react';
 import HomePageTop from '../../components/HomePageTop';
 import ImageSlider from "../../components/Slider/ImageSlider";
 import {SliderData} from "../../components/Slider/SliderData";
+import LoginLogic from "../../common/LoginLogic/LoginLogic";
 
 const HomePage:FC = () => {
     
     return (
         <div>
+            <LoginLogic/>
             <ImageSlider slides={SliderData}/>
             <HomePageTop id={0} type={''} brand={''} url={''} name={''} description={''} price={0} status={''}/>
         </div>
